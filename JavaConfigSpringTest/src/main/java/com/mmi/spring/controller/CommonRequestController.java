8e7e0890deb2a6f0c1bff4865c6d5d8bd7fb3320
jay4tech <jayprakashatcgc@gmail.com>
2018-03-31 12:17:09 +0530

    First Commit

---
+++ JavaConfigSpringTest/src/main/java/com/mmi/spring/controller/CommonRequestController.java
@@ -0,0 +1,13 @@
+package com.mmi.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CommonRequestController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexMethod() {
+        return "index";
+    }
+}
